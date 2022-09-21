7f1a48b8292a2f28adc11b6b408332f42c7a0dc2
Nick <nick@bloodandtreasure.com>
2022-09-21 19:11:27 +0300

    experiments with new features

--- app.js
+++ app.js
@@ -1,82 +1,45 @@
-//!Типи даних 
-/*
-Number
-String
-Boolean
+const t = 'ІЇХ2334452341235123451234514351345143';
+const regPasport = /^\p{L}{2}\d{6}$/u;
+const regMistPhone = /[\s\(\)\-\+]/g;
+const regPhone = /^\d{12}$/;
+const errors = ['Hello', 'this', 'is'];
 
-null
-undefined
+const mResult = regPasport.test(t);
+// console.log(mResult);
+// console.log(errors.toString());
 
-Object
+let a = 0;
+let b = 33;
+b &&= a;
+// console.log(b);
+let c = 3;
+let d = 0;
+d ||= c;
+// console.log(d);
+let e = 6;
+let f = 0;
+f ??= e;
+// console.log(f);
 
-Symbol
-BigInt
-*/
+const user = {
+    name : 'Nick',
+    // sayHi : {}
+}
 
+// console.log(user.age);
+// if(user.sayHi && typeof user.sayHi == 'object'){
+//     console.log(user.sayHi());
+// }
+// console.log(user.sayHi?.());
+// console.log('Some code!!!!!!!!!!!!!!!');
 
-//? Копіювання по значенню
-let n1 = 100;
-let n2 = n1;
-n1 = 200;
+function hello(){
+    const user = {
+        name : 'inner User'
+    };
 
-console.log(n1, n2);//200, 100
+    console.log(user);
+    console.log(globalThis.user);
+}
 
-//? Копіювання по посиланню
-const o1 = {
-    name : 'Nick'
-};
-
-const o2 = o1;
-
-o1.name = 'ne Nick';
-
-console.log(o1, o2);
-console.log(o1 === o2);
-
-// let vs var
-var1 = 'hello';
-console.log(var1);
-var var1 = 'jsdklfjsdfds';
-console.log(var1);
-
-// city = 'jjj';
-
-// let city;
-
-//let vs const
-const o3 = {
-    age : 100
-};
-
-o3.age = 34343;
-o3.n = 55;
-delete o3.age;
-
-//Рядки
-// "..." '...' `...`
-
-const r = "Hello, it is////////////////// a \"ROBOT\"";
-"Hello, it is a 'ROBOT'"
-
-console.log(r);
-
-//Шаблонні рядки
-//Template strings
-
-const greeting = `Hello ${ 5 * 6 - 9 + 22 }, my name is ${ o1.name }`;
-
-console.log(greeting);
-
-//Конкатенація 
-
-const s1 = 'Hell';
-const s2 = 'o';
-
-console.log(s1 + s2 + s1 + s2 + s1 + s1);
-console.log(`${ s1 + s2 }${ s2 }`);
-
-console.log('%cHello', 'color: red; padding: 25px; background-color: white;');
-
-// alert('Hello');
-// const n = prompt('Enter your name', 'Ivan');
-// const isHelp = confirm('Do you need a help?');
+hello();
